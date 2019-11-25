753fe44f071d41e406fdc5f6f26dfff7455baf77
dev-prime99 <prime99@icloud.com>
2019-11-25 16:17:11 +0100

    Milestone 1 completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ $("#btn_search_bar").click(function (){
   //Save text input
   var input = $("#txt_search_bar").val();
   console.log(input);
+        $("main").empty();
 
   $.ajax({
     url: "https://api.themoviedb.org/3/search/movie?api_key=ddb3139914f58732e5f61e64693b1c8f&query=" + input + "&page=1&include_adult=false",
@@ -13,17 +14,15 @@ $("#btn_search_bar").click(function (){
       var num_film = data.results.length
 
       for (var i = 0; i < num_film; i++) {
-        console.log(data)
 
         var source = $(".media-global").text()
         var template = Handlebars.compile(source)
-        console.log(data.results[i].poster_path);
 
         var globalmedia = {
-					title : data.response[i].title,
-					original_title : data.response[i].original_title,
-					original_language : data.response[i].original_language,
-					vote_average : data.response[i].vote_average
+					title : data.results[i].title,
+					original_title : data.results[i].original_title,
+					original_language : data.results[i].original_language,
+					vote_average : data.results[i].vote_average
 				}
 
         var html = template(globalmedia);
