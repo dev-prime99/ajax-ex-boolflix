758fa052123a0ff27d03a6b6b057ef7d77dc08d5
dev-prime99 <prime99@icloud.com>
2019-11-26 12:28:46 +0100

    stars added

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $("#btn_search_bar").click(function (){
   //Save text input
   var input = $("#txt_search_bar").val();
   console.log(input);
-        $("main").empty();
+  $("main").empty();
 
   $.ajax({
     url: "https://api.themoviedb.org/3/search/movie?api_key=ddb3139914f58732e5f61e64693b1c8f&query=" + input + "&page=1&include_adult=false",
@@ -21,12 +21,30 @@ $("#btn_search_bar").click(function (){
         var globalmedia = {
 					title : data.results[i].title,
 					original_title : data.results[i].original_title,
-					original_language : data.results[i].original_language,
-					vote_average : data.results[i].vote_average
+					original_language : data.results[i].original_language
 				}
 
         var html = template(globalmedia);
-        $("main").append(html)
+        $("main").append(html);
+
+        //assegnazione stelle valutazione
+        var vote = data.results[i].vote_average;
+        var halfvote = parseInt(Math.floor(vote / 2));
+        console.log(halfvote);
+
+        if (halfvote < 1) {
+          $(".media:last-child .vote").append('<i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>')
+        }else if (halfvote == 1) {
+          $(".media:last-child .vote").append('<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>')
+        }else if (halfvote == 2) {
+          $(".media:last-child .vote").append('<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>')
+        }else if (halfvote == 3) {
+          $(".media:last-child .vote").append('<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>')
+        }else if (halfvote == 4) {
+          $(".media:last-child .vote").append('<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>')
+        }else if (halfvote == 5) {
+          $(".media:last-child .vote").append('<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>')
+        }
       }
       console.log(num_film);
     },
